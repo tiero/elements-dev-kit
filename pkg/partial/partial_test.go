a078de1a727fb6b2991ca0c67cee131c1279d1f6
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-06-22 11:19:53 +0200

    Pass Network in NewPartial (#1)

--- pkg/partial/partial_test.go
+++ pkg/partial/partial_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/vulpemventures/go-elements/network"
 	"github.com/vulpemventures/go-elements/pset"
 	"github.com/vulpemventures/go-elements/transaction"
 )
@@ -12,9 +13,9 @@ const EMPTYPSET = "cHNldP8BAAsCAAAAAAAAAAAAAAA="
 
 func TestNewPartial(t *testing.T) {
 	emptyPset, _ := pset.New([]*transaction.TxInput{}, []*transaction.TxOutput{}, 2, 0)
-	want := &Partial{Data: emptyPset}
+	want := &Partial{Data: emptyPset, Network: &network.Liquid}
 
-	if got := NewPartial(); !reflect.DeepEqual(got, want) {
+	if got := NewPartial(nil); !reflect.DeepEqual(got, want) {
 		t.Errorf("NewPartial() = %v, want %v", got, want)
 	}
 	if gotB64, _ := want.Data.ToBase64(); gotB64 != EMPTYPSET {
